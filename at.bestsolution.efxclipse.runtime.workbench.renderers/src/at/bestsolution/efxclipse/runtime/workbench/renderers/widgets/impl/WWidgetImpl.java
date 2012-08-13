f049353d30934d38abc324160bc881fa40642c55
Tom Schindl <tom.schindl@bestsolution.at>
2012-08-14 01:02:13 +0200

    clean up

--- at.bestsolution.efxclipse.runtime.workbench.renderers/src/at/bestsolution/efxclipse/runtime/workbench/renderers/widgets/impl/WWidgetImpl.java
+++ at.bestsolution.efxclipse.runtime.workbench.renderers/src/at/bestsolution/efxclipse/runtime/workbench/renderers/widgets/impl/WWidgetImpl.java
@@ -66,7 +66,10 @@ public abstract class WWidgetImpl<N,M extends MUIElement> implements WWidget<M> 
 		if( nativeWidget != null ) {
 			setUserData(null);
 		}
+		propertyChangeHandler = null;
+		domElement = null;
 		activationCallbacks.clear();
+		activationCallbacks = null;
 	}
 	
 	@Override
